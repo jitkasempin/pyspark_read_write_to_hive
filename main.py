b81799e5928c39704b55369d2646189256d003dc
jitkasem pintaya <jitkasem.pin@ascendcorp.com>
2018-11-11 15:32:20 +0700

    Add code to ingest data to mySql on AWS RDS
    OK

--- main.py
+++ main.py
@@ -1,12 +1,9 @@
 from pyspark import SparkContext
-
 #import findspark
-
 from pyspark.sql import SQLContext
 from pyspark.sql import SparkSession
-
+from pyspark.sql.types import *
 # import configparser
-
 import argparse
 
 def use_pip_modules(spark_context):
@@ -17,19 +14,23 @@ def use_pip_modules(spark_context):
     hadoop_conf.set("fs.s3n.awsSecretAccessKey", "your_aws_access_key")
 
     sqlContext = SQLContext(spark_context)
+    
+    schema = StructType([
+        StructField("ethPri",FloatType(),True),
+        StructField("timeRecord",StringType(),True)
+    ])
 
-    df = sqlContext.read.json("s3n://bigdata-testing123/ethPrice_3.json")
+    df = sqlContext.read.json("s3n://bigdata-testing123/ethPrice_3.json", schema=schema)
+    
     df.show()
 
+    df.write.format("jdbc").option("url","jdbc:mysql://jitkasem-mysql.cmmw1yeaonrt.us-east-1.rds.amazonaws.com:3306/jitkasem?user=jitkasem&password=jitkasem341").option("driver", "com.mysql.jdbc.Driver").option("dbtable", "eth_table").mode("overwrite").save()
 
 
 if __name__ == '__main__': 
 
-
-
     sc = SparkContext(appName="S3 Test")
 
     use_pip_modules(sc)
-
 
     sc.stop()
