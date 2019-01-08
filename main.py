9405c3d56e0d89d5d3aaa022116f899ac811861f
jitkasem pintaya <jitkasem.pin@ascendcorp.com>
2019-01-08 14:14:56 +0700

    fix addition field
    fix addition field kub

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ from os.path import abspath, join
 from pyspark import SparkContext
 #import findspark
 from pyspark.sql import SQLContext
-#from pyspark.sql import
+from pyspark.sql import functions as F
 from pyspark.sql import SparkSession
 from pyspark.sql.types import *
 # import configparser
@@ -32,7 +32,7 @@ def use_pip_modules(spark_session):
 
     #spark_session.sql("CREATE TABLE IF NOT EXISTS transactionals (basket_id STRING, cust_code STRING, store_code STRING, prod_code STRING, quantity INT, spend FLOAT) PARTITIONED BY (shop_date STRING, shop_hour STRING) ROW FORMAT DELIMITED FIELDS TERMINATED BY ','")
     
-    spark_session.sql("CREATE TABLE IF NOT EXISTS datatest (SHOP_DATE STRING, SHOP_HOUR STRING, BASKET_ID STRING, CUST_CODE STRING, STORE_CODE STRING, PROD_CODE STRING, QUANTITY INT, SPEND FLOAT) STORED AS PARQUET")
+    spark_session.sql("CREATE TABLE IF NOT EXISTS datatest (DT STRING, SHOP_HOUR STRING, BASKET_ID STRING, CUST_CODE STRING, STORE_CODE STRING, PROD_CODE STRING, QUANTITY INT, SPEND FLOAT) STORED AS PARQUET")
 
     df = spark_session.read.format("csv").option("header", "true").schema(schema).load("/home/ubuntu/the-test-poc/supermarket_data.csv")
     
@@ -52,7 +52,13 @@ def use_pip_modules(spark_session):
 
     missing_removed.createOrReplaceTempView("transaction_record")
 
-    final_datadf = spark_session.sql("SELECT SHOP_DATE, SHOP_HOUR, BASKET_ID, CUST_CODE, STORE_CODE, PROD_CODE, QUANTITY, SPEND FROM transaction_record")
+    final_datadf = spark_session.sql("SELECT from_unixtime(unix_timestamp(SHOP_DATE,'yyyyMMdd'),'yyyy-MM-dd') AS DT, SHOP_HOUR, BASKET_ID, CUST_CODE, STORE_CODE, PROD_CODE, QUANTITY, SPEND FROM transaction_record")
+
+    final_datadf = final_datadf.withColumn('YY', F.split(final_datadf['DT'], '-')[0])
+    final_datadf = final_datadf.withColumn('MM', F.split(final_datadf['DT'], '-')[1])
+    final_datadf = final_datadf.withColumn('DD', F.split(final_datadf['DT'], '-')[2])
+
+    final_datadf.show()
 
     #spark_session.sql("INSERT INTO TABLE dataraws PARTITION (STORE_CODE) SELECT SHOP_DATE, SHOP_HOUR, BASKET_ID, CUST_CODE, PROD_CODE, QUANTITY, SPEND, STORE_CODE FROM transaction_record")
 
